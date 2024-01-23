6665c0aaf841e80651dc964a2d3c14692c05d2aa
decla <decla@DECLAN>
2024-01-23 01:14:57 +0000

    Add project files.

---
+++ RandomCardPickerMAUI/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace RandomCardPickerMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PickCardsButton_Clicked(object sender, EventArgs e)
+        {
+            if(int.TryParse(NumberOfCards.Text,out int amountOfCards))
+            {
+                string[] cardsPicked = CardPicker.PickSomeCards(amountOfCards);
+
+                foreach(string card in cardsPicked)
+                {
+                    PickedCards.Text = PickedCards.Text + Environment.NewLine + card;
+                }
+            }
+            else
+            {
+                PickedCards.Text = "Please Enter A Valid Number";
+            }
+        }
+    }
+
+}
---
+++ RandomCardPickerMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RandomCardPickerMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
